c46d828f98dd8fe0e4ae58085e75f94a9306c835
Kapral-Yrii <kapral2701@gmail.com>
2021-10-05 02:06:47 +0300

    update component FriendList and add component FriendListItem

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,18 +1,11 @@
 import PropTypes from 'prop-types'
 import style from './FriendList.module.css'
+import { FriendListItem } from '../FriendListItem/FriendListItem'
 
 export function FriendList({ friends }) {
     return (
         <ul className={style.list}>
-            {friends.map(el => {
-                return (
-                    <li className={style.item} key={el.id}>
-                        <span className={el.isOnline === true ? style.online : style.offline}></span>
-                        <img className={style.avatar} src={el.avatar} alt={el.name} width="48" />
-                        <p className="name">{el.name}</p>
-                    </li>
-                )
-            })}
+            <FriendListItem friends={friends} />
         </ul>
     )
 }
